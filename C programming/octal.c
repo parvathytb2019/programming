7ac49f3d1367a415841d9e0775908a543d213b99
parvathytb2019 <47378860+parvathytb2019@users.noreply.github.com>
2021-06-29 20:29:43 +0530

    Create octal.c

---
+++ C programming/octal.c
@@ -0,0 +1,16 @@
+  
+#include<stdio.h>
+void main()
+{
+	int num,rem,rev=0,count=1;
+	printf("enter a number:");
+	scanf("%d",&num);
+	while(num!=0)
+	{
+		rem=num%8;
+		rev=rev+(rem*count);
+		count=count*10;
+		num=num/8;
+	}
+	printf("octal value is%d",rev);
+}
