47259cfc476172af348ccd13362b139f420f65fb
parvathytb2019 <47378860+parvathytb2019@users.noreply.github.com>
2021-06-29 20:25:53 +0530

    Create binary.c

---
+++ C programming/binary.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void binary(int num);
+void main()
+{
+	int num;
+	printf("enter a number:");
+	scanf("%d",&num);
+	binary(num);
+}
+void binary(int num)
+{
+	int rem,rev=0,count=1;
+	while(num!=0)
+	{
+		rem=num%2;
+		rev=rev+(rem*count);
+		count=count*10;
+		num=num/2;
+	}
+	printf("binary value is%d",rev);
+}
