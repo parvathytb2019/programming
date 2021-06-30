0a6feefc66231154450983c131d7bc67fd74bf35
parvathytb2019 <47378860+parvathytb2019@users.noreply.github.com>
2021-06-30 20:09:01 +0530

    Create efficency.c

---
+++ C programming/efficency.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+void main()
+{
+	float time;
+	printf("enter the working hours:");
+	scanf("%f",&time);
+	if((time>=2.0) && (time<3.0))
+		printf("highly efficent worker");
+	if((time>=3.0) && (time<4.0))
+		printf("worker needs to improve speed");
+	if((time>=4.0) && (time<5.0))
+		printf("need training to improve the worker's speed");
+	if(time>5.0)
+		printf("the worker will be terminated");
+}
